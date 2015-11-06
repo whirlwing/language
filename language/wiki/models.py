55c526302570f4fd86a50eb9d486331a1e9d1114
m516 <m516@m516>
2015-11-06 22:31:50 +0800

    07 over

--- language/wiki/models.py
+++ language/wiki/models.py
@@ -3,13 +3,15 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField()
+    likes = models.IntegerField()
 
     def __str__(self):
         return self.name
 
 class Page(models.Model):
     category = models.ForeignKey(Category)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=128, unique=True)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
