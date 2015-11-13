05f7889b9a763d4b8da48216ec6049237f032d5b
m516 <m516@m516>
2015-11-14 00:27:12 +0800

    070809

--- language/wiki/models.py
+++ language/wiki/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
+import random
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    views = models.IntegerField()
-    likes = models.IntegerField()
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    
+    def save(self, *args, **kwargs):
+        self.name = self.name.replace(' ', '-')
+        super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -14,6 +20,12 @@ class Page(models.Model):
     title = models.CharField(max_length=128, unique=True)
     url = models.URLField()
     views = models.IntegerField(default=0)
+
+
+    def save(self, *args, **kwargs):
+        self.title = self.title.replace(' ', '-')
+        super(Page, self).save(*args, **kwargs)
+
 
     def __str__(self):
         return self.title# Create your models here.
--- language/wiki/views.py
+++ language/wiki/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from wiki.forms import CategoryForm
+from wiki.models import Category, Page
 
 def wiki(request):
-    context = {}
+    categories = Category.objects.order_by('-likes')
+    context = {'categories':categories}
     return render(request, 'wiki/wiki.html', context)
+
+def category(request, categoryName):
+    context = {}
+    try:
+        category = Category.objects.get(name=categoryName)
+        context['category'] = category
+        context['pages'] = Page.objects.filter(category=category)
+    except Category.DoesNotExist:
+        pass
+    return render(request, 'wiki/category.html', context)
+
+
+def addCategory(request):
+    template = 'wiki/addCategory.html'
+    if request.method=='GET':
+        return render(request, template, {'form':CategoryForm()})
+    # request.method=='POST'
+    form = CategoryForm(request.POST)
+    if not form.is_valid():
+        return render(request, template, {'form':form})
+    form.save()
+    return redirect(reverse('wiki:wiki'))
+    # Or try this: return wiki(request)
 
 
 def about(request):
